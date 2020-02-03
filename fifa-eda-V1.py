ba8fae9706dba0871c3a3b321804c4423c1541de
Verma <212677783@logon.ds.ge.com>
2020-02-03 18:10:58 +0800

    Data Cleaning

--- fifa-eda-V1.py
+++ fifa-eda-V1.py
@@ -45,10 +45,42 @@ df_null_cols=[x.replace(' ','_') for x in df_null_cols]
 # For now lets separate them to a new data frame and remove them from our main dataset (df)
 df_noclub=df[df.Club.isnull()]
 df=df.dropna(subset=['Club'])
-
+df.reset_index(drop=True)
 # Goal keeps dont have attributes such as LS, ST, RS, etc attribute. Hence they all have null values in them.
 # So lets create another dataframe for position='GK'.
 # In future analysis when all players are analyses together, we dont considered such columns.
 df_GK=df[df['Position']=='GK']
-#for i in df_null_cols:
-#    df_name='df_'+df_null_cols[i]+'_null'
+
+# Anlysing null values in Preferred Foot Column
+df_PF_null=df[df['Preferred Foot'].isnull()]
+# Analysing this, we see there are 48 such players and all have potential of 62.
+# They have no data relevent to their positional skills and physical attributes. We can remove them.
+# This will remove null values in many columns which we will see next.
+df=df.dropna(subset=['Preferred Foot'])
+del df_PF_null
+
+# Lets check null values again
+df_null_count=df.isna().sum()
+df_null_count=df_null_count[df_null_count>0]
+# No alanlysing the remaining pattern of null values, there is a logical pattern.
+# If a player is loaned from a club, its 'Joined' and 'Release Clause' column is logically blank.
+# This is verfied by counting total of null values in 'Loaned From' and 'Joined' column which sums up to total data points.
+# Lets have a look the freq of the clubs which Loaned players.
+df['Loaned From'].value_counts()
+# There are 341 clubs that have loaned players and Atalanta has loaned a maximum of 20 players.
+
+# Further analysing, we see that the players which have valid data in Loaned from column have contract valid till a date in 2018 or 2019.
+# This is old info, now lets think they were retained or bought by their current clubs. Hence 'Loaned From' column is not needed now.
+# For these players, lets set their contracts valid till data to be 2 years from now. So, lets set it 2022.
+# Also, for those players, lets set the 'Joined' date to be Jul 1, 2019. Assuming deal happened mid last year.
+
+# Now, for the 'Release Clause' column, lets set their value as the mean of the values of that player's rating.
+
+df=df.drop(['Loaned From'],axis=1)
+
+for i in range(len(df)):
+    a=pd.isnull(df['Joined'][i])
+    if a==True:
+        df['Joined'][i]='Jul 1, 2019'
+        df['Contract Valid Until'][i]='2022'
+    else: continue
