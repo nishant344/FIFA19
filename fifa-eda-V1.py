60a9ab4fe7dda398867849d927bed41d73f2df82
Nishant <nishant.344@gamil.com>
2020-02-04 21:06:48 +0800

    Null Value Imputation

--- fifa-eda-V1.py
+++ fifa-eda-V1.py
@@ -45,7 +45,8 @@ df_null_cols=[x.replace(' ','_') for x in df_null_cols]
 # For now lets separate them to a new data frame and remove them from our main dataset (df)
 df_noclub=df[df.Club.isnull()]
 df=df.dropna(subset=['Club'])
-df.reset_index(drop=True)
+df=df.reset_index(drop=True)
+
 # Goal keeps dont have attributes such as LS, ST, RS, etc attribute. Hence they all have null values in them.
 # So lets create another dataframe for position='GK'.
 # In future analysis when all players are analyses together, we dont considered such columns.
@@ -57,12 +58,13 @@ df_PF_null=df[df['Preferred Foot'].isnull()]
 # They have no data relevent to their positional skills and physical attributes. We can remove them.
 # This will remove null values in many columns which we will see next.
 df=df.dropna(subset=['Preferred Foot'])
+df=df.reset_index(drop=True)
 del df_PF_null
 
 # Lets check null values again
 df_null_count=df.isna().sum()
 df_null_count=df_null_count[df_null_count>0]
-# No alanlysing the remaining pattern of null values, there is a logical pattern.
+# Now alanlysing the remaining pattern of null values, there is a logical pattern.
 # If a player is loaned from a club, its 'Joined' and 'Release Clause' column is logically blank.
 # This is verfied by counting total of null values in 'Loaned From' and 'Joined' column which sums up to total data points.
 # Lets have a look the freq of the clubs which Loaned players.
@@ -74,13 +76,30 @@ df['Loaned From'].value_counts()
 # For these players, lets set their contracts valid till data to be 2 years from now. So, lets set it 2022.
 # Also, for those players, lets set the 'Joined' date to be Jul 1, 2019. Assuming deal happened mid last year.
 
-# Now, for the 'Release Clause' column, lets set their value as the mean of the values of that player's rating.
-
 df=df.drop(['Loaned From'],axis=1)
 
 for i in range(len(df)):
-    a=pd.isnull(df['Joined'][i])
-    if a==True:
-        df['Joined'][i]='Jul 1, 2019'
-        df['Contract Valid Until'][i]='2022'
+#    a=pd.isnull(df['Joined'].iloc[i])
+    if pd.isnull(df['Joined'])[i]==True:
+        df['Joined'].iloc[i]='Jul 1, 2019'
+        df['Contract Valid Until'].iloc[i]='2022'
     else: continue
+
+# Now, for the 'Release Clause' column, lets set their value as the mean of the values of that player's rating.
+# For this, first we need to convert the data in this column to a numeric value.
+# We will remove '€' symbol and tackle 'M' and "K' string with correct multiplication factors.
+df['Release Clause']=df['Release Clause'].replace('[\€]', '', regex=True)
+
+for i in range(len(df['Release Clause'])):
+    if pd.isnull(df['Release Clause'].iloc[i])==False:
+        release_val=df['Release Clause'][i][:-1]
+        if df['Release Clause'][i][-1]=='M':
+            factor=pow(10,6)
+        elif df['Release Clause'][i][-1]=='K':
+            factor=pow(10,3)
+        else: continue
+        df['Release Clause'][i]=pd.to_numeric(release_val)*factor
+    else: continue
+    
+df['Release Clause'].astype(float)
+df[['Release Clause','Overall']].groupby(['Overall']).max()
